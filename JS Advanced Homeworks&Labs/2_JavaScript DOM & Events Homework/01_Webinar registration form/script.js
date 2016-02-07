837c5c796e5bc7be84bb02f13ec5f66e08595499
aanguelov <aanguelov@gmail.com>
2016-02-07 19:16:37 +0200

    JavaScript DOM & Events Homework add first problem

---
+++ JS Advanced Homeworks&Labs/2_JavaScript DOM & Events Homework/01_Webinar registration form/script.js
@@ -0,0 +1,6 @@
+var invoiceDetails = document.getElementById('invoiceDetails');
+var btn = document.getElementById('invoice');
+
+btn.onchange = function() {
+    invoiceDetails.style.visibility = 'visible';
+};
