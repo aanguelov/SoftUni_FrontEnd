ef72909601f83364cff5887e5720fba82abf79d1
aanguelov <aanguelov@gmail.com>
2016-04-19 22:24:56 +0300

    Implemented view issue page.

--- JS Frameworks Angular Project/app/js/controllers/issuesController.js
+++ JS Frameworks Angular Project/app/js/controllers/issuesController.js
@@ -1,7 +1,29 @@
 angular.module('issueTracker.controllers.issues', [])
     .config(['$routeProvider', function($routeProvider) {
-
+        $routeProvider
+            .when('/issues/:id', {
+                templateUrl: 'partials/issue-page.html',
+                controller: 'ViewIssueController'
+            })
+            .when('/issues/edit/:id', {
+                templateUrl: 'partials/edit-issue.html',
+                controller: 'EditIssueController'
+            })
     }])
-    .controller('ViewIssueController', ['$scope', function($scope) {
+    .controller('ViewIssueController', ['$scope', '$routeParams', 'issues', function($scope, $routeParams, issues) {
+        issues.getIssueById($routeParams.id)
+            .then(function success(data) {
+                console.log(data);
+                $scope.currentIssue = data;
+                $scope.currentIssueLabels = [];
+
+                data.Labels.forEach(function(label) {
+                    $scope.currentIssueLabels.push(label.Name);
+                })
+            }, function error(err) {
+
+            });
+    }])
+    .controller('EditIssueController', ['$scope', '$routeParams', 'issues', function($scope, $routeParams, issues) {
 
     }]);
--- JS Frameworks Angular Project/app/js/services/issues-service.js
+++ JS Frameworks Angular Project/app/js/services/issues-service.js
@@ -23,7 +23,29 @@ angular.module('issueTracker.services.issues', [])
             return deferred.promise;
         }
 
+        function getIssueById(id) {
+            var deferred = $q.defer();
+
+            var req = {
+                method: 'GET',
+                url: baseUrl + 'issues/' + id,
+                headers: {
+                    'Authorization': 'Bearer ' + JSON.parse(sessionStorage['currentUser']).access_token
+                }
+            };
+
+            $http(req)
+                .then(function success(response) {
+                    deferred.resolve(response.data);
+                }, function error(err) {
+                    deferred.reject(err);
+                });
+
+            return deferred.promise;
+        }
+
         return {
-            getUserIssues: getMyIssues
+            getUserIssues: getMyIssues,
+            getIssueById: getIssueById
         }
     }]);
