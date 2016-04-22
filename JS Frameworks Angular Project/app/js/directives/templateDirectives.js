293ab52465c69e3d7d9840b57d7ea8d12b83b9f5
aanguelov <aanguelov@gmail.com>
2016-04-22 13:28:31 +0300

    Added more directives.

--- JS Frameworks Angular Project/app/js/directives/templateDirectives.js
+++ JS Frameworks Angular Project/app/js/directives/templateDirectives.js
@@ -16,4 +16,34 @@ angular.module('issueTracker.directives.filters', [])
             restrict: 'A',
             templateUrl: 'app/partials/issues/issueFilter.html'
         }
+    }])
+    .directive('ngIssueCommentForm', [function() {
+        return {
+            restrict: 'A',
+            templateUrl: 'app/partials/issues/issue-comment-form.html'
+        }
+    }])
+    .directive('ngIssueComments', [function() {
+        return {
+            restrict: 'A',
+            templateUrl: 'app/partials/issues/issue-comments.html'
+        }
+    }])
+    .directive('ngDashboard', [function() {
+        return {
+            restrict: 'A',
+            templateUrl: 'app/partials/dashboard.html'
+        }
+    }])
+    .directive('ngLoginUserForm', [function() {
+        return {
+            restrict: 'A',
+            templateUrl: 'app/partials/login.html'
+        }
+    }])
+    .directive('ngRegisterUserForm', [function() {
+        return {
+            restrict: 'A',
+            templateUrl: 'app/partials/register.html'
+        }
     }]);
